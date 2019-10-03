3491cbe2f03a6c48c217b78735580e2d8c587b17
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2019-10-03 18:01:03 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var number1 = BigInt(first);
+  var number2 = BigInt(second);
+  var x  = BigInt(number1*number2);
+
+  var str = x+'';
+  var result  = str.slice(0, str.length);
+  return result;
 }
